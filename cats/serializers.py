b2857ad551f55839a42dcaefa089e5bb8a71659b
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-02-24 20:55:07 +0300

    1

---
+++ cats/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Cat, Owner
+
+
+class CatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color', 'birth_year')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name') 
